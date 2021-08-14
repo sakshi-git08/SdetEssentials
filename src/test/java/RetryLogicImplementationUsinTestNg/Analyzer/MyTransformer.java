72d0c7a035197e812380d1e17502153061b6417d
Sakshi Aggarwal <sakshi.agg428@gmail.com>
2021-08-14 09:03:14 +0530

    ExtentReportsKnowledge

---
+++ src/test/java/RetryLogicImplementationUsinTestNg/Analyzer/MyTransformer.java
@@ -0,0 +1,16 @@
+package Analyzer;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class MyTransformer implements IAnnotationTransformer {
+    //Constructor and Method both are present in java.lang.reflect package
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor test, Method testMethod){
+
+        annotation.setRetryAnalyzer(RetryAnalyzer.class); // on this class I want to use retry mechanism
+
+    }
+}
---
+++ src/test/java/RetryLogicImplementationUsinTestNg/mytests/MyTests.java
@@ -0,0 +1,17 @@
+package mytests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyTests {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(false, true);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(false,true);
+    }
+}
